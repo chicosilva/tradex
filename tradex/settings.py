01b4fcabd5eaee076214d4b5e78bd8dc15657d57
francisco <chicosilva1@gmail.com>
2023-10-05 11:14:23 -0300

    add Swagger

--- tradex/settings.py
+++ tradex/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "apps.users",
     "apps.products",
     "rest_framework_simplejwt",
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- tradex/urls.py
+++ tradex/urls.py
@@ -6,11 +6,27 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 
 from apps.products.views import PriceVariationViewSet, ProductViewSet
 from apps.users.views import RegisterViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your Project API",
+        default_version='v1',
+        description="Description of your API",
+        terms_of_service="https://www.yoursite.com/terms/",
+        contact=openapi.Contact(email="contact@yoursite.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 
@@ -26,5 +42,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('healthcheck/', health_check, name='health_check'),
+    path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
